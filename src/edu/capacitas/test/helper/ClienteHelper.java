2931bb720b1534ee2252836bf1381d8a2e2ff173
gauchoaleman <gauchoaleman@yahoo.com>
2016-10-21 21:37:21 -0300

    Trabajando con ClienteHelper

--- src/edu/capacitas/test/helper/ClienteHelper.java
+++ src/edu/capacitas/test/helper/ClienteHelper.java
@@ -2,24 +2,18 @@ package edu.capacitas.test.helper;
 
 import edu.capacitas.domain.Cliente;
 
-import java.util.Scanner;
-
 /**
  * Created by luks on 17/10/16.
  */
 public class ClienteHelper {
-
-    Scanner tecladoCliente;
-
-    public ClienteHelper() {
-
-        this.tecladoCliente = new Scanner(System.in);
-    }
-
     public static void crearCliente() {
         System.out.println("Arranca Crear Cliente");
         Cliente cliente = new Cliente();
         cliente.setNombre("Stefan");
+        cliente.setApellido("Ronacher");
+        cliente.setEmail("gauchoaleman@yahoo.com");
+        cliente.setDni("92844240");
+        cliente.setSexo("MASCULINO");
 
         System.out.println(cliente);
     }
